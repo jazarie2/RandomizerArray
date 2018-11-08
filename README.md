543740c1eb7474a85f2bbf5cbb6529c0e348ed42
Abdul Aziz <Abdul_Aziz@jabil.com>
2018-11-08 11:56:38 +0800

    Working basic functionality as described

--- README.md
+++ README.md
@@ -4,7 +4,17 @@ a simple python snip to get a sequential number (i.e. 0~100) and split it uneven
 Python snipets to get a list of randomize number that will deviate +3 and -3 from min of the total value split +3(Total/Split)-3
 usage
 
-from RandomizerArray import Randomizer as RA
+#require numpy
 
+from RandomizerArray import Randomizer as RA
 q = RA(maxval=40, split=4)
-q.placement (To get result)
+q.placement #(To get result)
+
+example of result:
+[
+	[21, 13, 5, 36, 30, 32, 6, 7, 40], 
+	[10, 29, 1, 20, 3, 15, 34, 35, 17, 9, 26], 
+	[4, 39, 38, 22, 27, 11, 33], 
+	[2, 37, 8, 12, 14, 16, 18, 19, 23, 24, 25, 28, 31]
+]
+
---
+++ RandomizerArray.py
@@ -0,0 +1,26 @@
+import numpy as np
+class Randomizer:
+	def __init__(self, maxval=24, split=3):
+		print("MAX VALUE: "+str(maxval) )
+		self.maxval = maxval
+		self.trial = split
+		self.stack = range(1,self.maxval+1)
+		self.placement = []
+		self.minlimit = int(round((self.maxval/self.trial/(self.trial+1))))
+		self.maxlimit = int(round((self.stack.__len__()/self.trial)))
+		self.get()
+	def get(self):
+		self.stack = range(1,self.maxval+1)
+		self.placement = []
+		for x in range(1, self.trial):	
+			if (x<self.trial):
+				self.maxlimit = int(round((self.maxval/(self.trial))))+3
+				self.minlimit = self.maxval/(self.trial)-3
+				howmany = np.random.randint(self.minlimit,self.maxlimit)		
+				t = list(np.random.choice(self.stack,howmany,replace=False))				
+				self.placement.append(t)
+				self.stack = list(set(self.stack) - set(t))
+		self.placement.append(self.stack)
+		for q in self.placement:
+			print "total:" + str(q.__len__()) +" ==> "+ str(q)
+		return np.sort(self.placement)
